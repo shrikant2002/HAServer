6957b091d8a036c9c743e1c8db910ce486fedfbe
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-12-18 16:20:07 +0530

    Update and rename install2.sh to install3.sh

--- install3.sh
+++ install3.sh
@@ -9,9 +9,9 @@ pkg install mosquitto -y
 pkg install openssh -y
 pkg install termux-api -y
 apt install make -y
-pip install PyNaCl==1.3.0 -y
-pip install aiohttp_cors -y
-pip install homeassistant -y
+yes | pip install PyNaCl==1.3.0 -y
+yes | pip install aiohttp_cors -y
+yes | pip install homeassistant -y
 pkg install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
-LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" pip install --upgrade wheel Pillow==7.2.0 -y
+LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" yes | pip install --upgrade wheel Pillow==7.2.0
 hass -v
