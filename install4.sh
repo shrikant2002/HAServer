c2d01c19917a01c6cf6129145ddd0a32f7288d6f
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-12-18 16:47:10 +0530

    Update and rename install3.sh to install4.sh

--- install4.sh
+++ install4.sh
@@ -8,10 +8,10 @@ pkg install nano -y
 pkg install mosquitto -y
 pkg install openssh -y
 pkg install termux-api -y
-apt install make -y
-yes | pip install PyNaCl==1.3.0 -y
-yes | pip install aiohttp_cors -y
-yes | pip install homeassistant -y
 pkg install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
+apt install make -y
+yes | pip install PyNaCl==1.3.0
+yes | pip install aiohttp_cors
+yes | pip install homeassistant
 LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" yes | pip install --upgrade wheel Pillow==7.2.0
 hass -v
